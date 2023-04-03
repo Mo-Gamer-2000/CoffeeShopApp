a06ff888db5ddb699d20fdc71576f2db8136a826
Moeez Abdul <MoeezRajput2000@Gmail.com>
2023-04-03 13:25:03 +0100

    Display welcome, available coffee sizes with prices and types of coffee messages.
    With user input!

--- CoffeeShopApp/Main.cpp
+++ CoffeeShopApp/Main.cpp
@@ -102,6 +102,62 @@ public:
 };
 
 int main() {
-	std::cout << "Hello, from the Coffee Shop!" << std::endl;
+	std::cout << "Welcome to the Mo's Coffee Shop!" << std::endl;
+	std::cout << "Please select your coffee size: " << std::endl;
+	std::cout << "1. Small  ($2.00)" << std::endl;
+	std::cout << "2. Medium ($3.00)" << std::endl;
+	std::cout << "3. Large  ($4.00)" << std::endl;
+	std::cout << "Enter your choice (1-3): ";
+
+	int choice;
+	std::cin >> choice;
+
+	double base_price = 0.0;
+
+	if (choice == 1)
+		base_price = 2.00;
+	else if (choice == 2)
+		base_price = 3.00;
+	else if (choice == 3)
+		base_price = 4.00;
+
+	if (base_price == 0.0)
+	{
+		std::cout << "Invalid choice!" << std::endl;
+		return 1;
+	}
+
+	std::cout << "Please select your coffee type: " << std::endl;
+    std::cout << "1. Espresso" << std::endl;
+    std::cout << "2. Latte" << std::endl;
+    std::cout << "3. Cappuccino" << std::endl;
+    std::cout << "4. Americano" << std::endl;
+    std::cout << "5. Mocha" << std::endl;
+    std::cout << "Enter your choice (1-5): ";
+
+	std::cin >> choice;
+	std::string coffee_type;
+
+	switch (choice)
+	{
+	case 1:
+		coffee_type = "Espresso";
+		break;
+	case 2:
+		coffee_type = "Latte";
+		break;
+	case 3:
+		coffee_type = "Cappuccino";
+		break;
+	case 4:
+		coffee_type = "Americano"; 
+		break;
+	case 5:
+		coffee_type = "Mocha"; 
+		break;
+	default:
+		std::cout << "invalid choice!" << std::endl;
+		return 1;
+	}
 	return 0;
 }

d4bb20d395cd1ac03ea83827ba4e8aaf9f7c6c8e
Moeez Abdul <MoeezRajput2000@Gmail.com>
2023-04-03 12:53:55 +0100

    Empty classes created to structure the app

--- CoffeeShopApp/Main.cpp
+++ CoffeeShopApp/Main.cpp
@@ -3,8 +3,37 @@
 #include <memory>
 #include <string>
 
+class Coffee {
+public:
+private:
+};
+
+class Extras : public Coffee {
+public:
+private:
+};
+
+class Milk : public Extras {
+public:
+private:
+};
+
+class Sugar : public Extras {
+public:
+private:
+};
+
+class WhippedCream : public Extras {
+public:
+private:
+};
+
+class Cinnamon : public Extras {
+public:
+private:
+};
+
 int main() {
 	std::cout << "Hello, from the Coffee Shop!" << std::endl;
-
 	return 0;
 }
